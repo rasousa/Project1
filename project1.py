f6ed46e19aefaee0d8ebc9a4440e177204831752
Rebecca <rebecca.ann.sousa@gmail.com>
2018-09-07 13:10:33 -0600

    Hardcoded paths

--- project1.py
+++ project1.py
@@ -1,4 +1,43 @@
+#import pandas as pd
+import numpy as np
+import sys
+import os
+
+# USAGE: python project1.py training.csv testing.csv
+
+def processFile(path):
+    f = open(path, "r")
+    # Read into a numpy array
+    a = np.fromfile(f, sep=",")
+    f.close()
+    return a
+
+#def buildTree():
+    # TODO
+
+#def calcGini():
+    # TODO
+
+#def calcIG():
+    # TODO
+
+#def chiSquare():
+    # TODO
+
 def main():
+    # Get the training and testing file paths
+    #trainingPath = os.path.abspath(sys.argv[1])
+    #testingPath = sys.argv[2]
+
+    # Hardcoded paths
+    trainingPath = "/Users/rebeccasousa/Documents/UNM/CS529/Project1/training.csv"
+    testingPath = "/Users/rebeccasousa/Documents/UNM/CS529/Project1/testing.csv"
+
+    # Convert the training and testing files into numpy arrays
+    trainingData = processFile(trainingPath)
+    testingData = processFile(testingPath)
+
+    print(trainingData)
 
 if __name__ == '__main__':
     main()
