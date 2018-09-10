52ed9e347d902a18f82a520f338d94221652532e
Rebecca <rebecca.ann.sousa@gmail.com>
2018-09-09 18:10:33 -0600

    Code and psuedocode for ID3

--- project1.py
+++ project1.py
@@ -1,37 +1,86 @@
-#import pandas as pd
 import numpy as np
+import pandas as pd
+from anytree import Node, NodeMixin
 import sys
 import os
 
-# USAGE: python project1.py training.csv testing.csv
+# USAGE: python3 project1.py training.csv testing.csv
 
+### FILE PROCESSING ###
 def processFile(path):
     f = open(path, "r")
-    #line1 = f.readline()
 
     # Read into a numpy array
-    a = np.genfromtxt(f, dtype=None, delimiter=',')
+    a = np.genfromtxt(f, dtype=None, delimiter=',', encoding=None)
+
     f.close()
     return a
 
-#def buildTree():
-    # TODO
+### SPLIT CRITERIA ###
 
-#def calcGini():
-    # TODO
+def giniIndex():
+    result = 0
+    # handle target attributes with arbitrary labels
+    #dictionary = summarizeExamples(examples, targetAttribute)
+    #for key in dictionary:
+        #proportion = dictionary[key]/total number of examples
+        #result += proportion * proportion
+    return 1 - result
 
-#def calcIG():
-    # TODO
+def entropy(data):
+    result = 0
+    # handle target attributes with arbitrary labels
+    #dictionary = summarizeExamples(examples, targetAttribute)
+    #for key in dictionary:
+        #proportion = dictionary[key]/total number of examples
+        #result -= proportion * log2(proportion)
+    return result
+
+def infoGain(data):
+    gain = entropy(data)
+    #for value in attributeValues(examples, attribute):
+        #sub = subset(examples, attribute, value)
+        #gain -=  (number in sub)/(total number of examples) * entropy(sub)
+    return gain
+
+### STOP CRITERIA ###
 
 #def chiSquare():
     # TODO
+
+### ID3 TREE STRUCTURE ###
+
+class baseNode(object):
+    foo = 4
+
+class id3Node(baseNode, NodeMixin):
+    def __init__(self, parent=None, classify="None", attr=[], ig=0, chi=0):
+        self.parent = parent
+        self.classify = classify
+        self.attr = attr
+        self.ig = ig
+        self.chi = chi
+
+def buildTree(data, parent):
+    t = id3Node(parent)
+    #label(t) = representativeClass(data)
+    #if(impure(data))
+        #criterion = splitCriterion(data)
+    #else
+        #return t
+    #[D1, D2, ..., Dn] = decomposing(data, criterion)
+    #for D in [D]:
+        #buildTree(D, t) #build tree on split data with t as parent
+    #return t
+
+### MAIN ###
 
 def main():
     # Get the training and testing file paths
     #trainingPath = os.path.abspath(sys.argv[1])
     #testingPath = sys.argv[2]
 
-    # Hardcoded paths
+    # Hardcoded paths TODO: don't be hardcoded paths
     trainingPath = "/Users/rebeccasousa/Documents/UNM/CS529/Project1/training.csv"
     testingPath = "/Users/rebeccasousa/Documents/UNM/CS529/Project1/testing.csv"
 
@@ -39,8 +88,14 @@ def main():
     trainingData = processFile(trainingPath)
     testingData = processFile(testingPath)
 
-    print(trainingData)
-    print(testingData)
+    # Print for testing purposes
+    #print(trainingData)
+
+    # Build the decision tree
+    dt = buildTree(trainingData, None)
+
+    # Print for testing purposes
+    print(dt)
 
 if __name__ == '__main__':
     main()
